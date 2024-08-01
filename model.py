9d2c56f5607d86f24e48df95079a66deac083d44
S M Hasan Mansur <smhasanmansur@Ss-MacBook-Pro-2.local>
2024-07-31 20:23:06 -0400

    Multihead Attention added

--- model.py
+++ model.py
@@ -31,7 +31,7 @@ class PositionalEncoding(nn.Module):
         """
         Args:
             max_seq_len: max length of input sequence
-            embed_dim: demension of embedding
+            embed_dim: dimension of embeddings
             dropout: dropout rate
         """
         super().__init__()
@@ -60,7 +60,6 @@ class PositionalEncoding(nn.Module):
         Returns:
             x: output vector of shape [batch_size, seq_len, embed_dim]
         """
-
         # making embeddings relatively larger so that the positional encoding
         # becomes relatively smaller. This means the original meaning in the embedding
         # vector won’t be lost when we add them together.
@@ -71,20 +70,131 @@ class PositionalEncoding(nn.Module):
         # dropout for regularization
         return self.dropout(x)
 
-# ------ transformer encoder block --------------
-
 # multihead attention
+class MultiHeadAttention(nn.Module):
+    def __init__(self, embed_dim, num_heads=8, dropout=0.1):
+        """
+        Args:
+            embed_dim: dimension of embeddings
+            num_heads: number of self attention heads
+            dropout: dropout rate
+        """
+        super().__init__()
+        self.embed_dim = embed_dim
+        self.num_heads = num_heads
+        # dropout layer to prevent overfitting
+        self.dropout = nn.Dropout(dropout)
 
-# feed forward layer
+        # ensuring the embedding dimension is divisible by the number of heads
+        assert embed_dim % num_heads == 0, "embed_dim must be divisible by num_heads"
+        # dimension of each attention head's key, query, and value vectors
+        self.single_head_dim
+        return x.transpose(1, 2)= embed_dim // num_heads
+
+        # Defining the weight matrices
+        self.w_q = nn.Linear(embed_dim, embed_dim) # weight matrix for query
+        self.w_k = nn.Linear(embed_dim, embed_dim) # weight matrix for key
+        self.w_v = nn.Linear(embed_dim, embed_dim) # weight matrix for value
+        self.w_o = nn.Linear(embed_dim, embed_dim) # weight matrix for output
+
+    def split(self, x):
+        """
+        Args:
+            x: vector of shape [batch_size, seq_len, embed_dim]
+        Returns:
+            x: vector of shape [batch_size, num_heads, seq_len, single_head_dim]
+        """
+        batch_size, seq_len, embed_dim = x.size()
+        x = x.view(batch_size, seq_len, self.num_heads, self.single_head_dim)
+        # transpose to bring the head to the second dimension
+        # final shape (batch_size, num_heads, seq_len, single_head_dim)
+        return x.transpose(1, 2)
+
+    def combine(self, x):
+        """
+        Args:
+            x: vector of shape [batch_size, num_heads, seq_len, single_head_dim]
+        Returns:
+            x: vector of shape [batch_size, seq_len, embed_dim]
+        """
+        batch_size, num_heads, seq_len, single_head_dim = x.size()
+        embed_dim = num_heads * single_head_dim
+        x = x.transpose(1, 2).contiguous().view(batch_size, seq_len, embed_dim)
+        return x
+
+    def scaled_dot_product_attention(self, q, k, v, attention_mask=None, key_padding_mask=None, dropout=None):
+        """
+        Args:
+            q: vector of shape [batch_size, num_heads, query_sequence_length, single_head_dim]
+            k: vector of shape [batch_size, num_heads, key_sequence_length, single_head_dim]
+            v: vector of shape [batch_size, num_heads, key_sequence_length, single_head_dim]
+            attention_mask: vector of shape [query_sequence_length, key_sequence_length]
+            key_padding_mask : vector of shape [sequence_length, key_sequence_length]
+
+        Returns:
+            attention_values: vector of shape [batch_size, num_heads, seq_len, single_head_dim]
+            attention_scores: vector of shape [batch_size, num_heads, seq_len, single_head_dim]
+        """
+        single_head_dim = k.size(-1)
+        # tgt_len, src_len = q.size(-2), k.size(-2)
+
+        # dot product Query with Key^T to compute similarity
+        k_t = k.transpose(-2, -1) # transpose
+        attention_scores = (q @ k_t) / math.sqrt(d_tensor) # scaled dot product
+
+        # apply attention masking (opt)
+        if attention_mask is not None:
+            attention_scores = attention_scores.masked_fill(attention_mask == 0, -1e9)
+
+        # apply key padding masking (opt)
+        if key_padding_mask is not None:
+            key_padding_mask = key_padding_mask.unsqueeze(1).unsqueeze(2) # Broadcast over batch size, num heads
+            attention_scores = attention_scores + key_padding_mask
+
+        # softmax to make [0, 1] range
+        attention_scores = torch.softmax(attention_scores, dim=-1)
+
+        # apply dropout to prevent overfitting (opt)
+        if dropout is not None:
+            attention_scores = dropout(attention_scores)
+
+        # multiply with Value
+        attention_values = attention_scores @ v
+
+        return attention_values, attention_scores
+
+    def forward(self, q, k, v, attention_mask=None, key_padding_mask=None):
+        """
+        Args:
+            q: vector of shape [batch_size, query_sequence_length, embed_dim]
+            k: vector of shape [batch_size, key_sequence_length, embed_dim]
+            v: vector of shape [batch_size, key_sequence_length, embed_dim]
+            attention_mask: vector of shape [query_sequence_length, key_sequence_length]
+            key_padding_mask : vector of shape [sequence_length, key_sequence_length]
+
+        Returns:
+            x: vector of shape [batch_size, seq_len, embed_dim]
+        """
+        # dot product with weight matrices
+        q, k, v = self.w_q(q), self.w_k(k), self.w_v(v)
+        # split by number of heads
+        q, k, v = self.split(q), self.split(k), self.split(v)
+        # scale dot product to compute similarity
+        attention_values, attention_scores = self.scaled_dot_product_attention(q, k, v, attention_mask, key_padding_mask, self.dropout)
+        self.attention_scores = attention_scores
+        # combine and pass to linear layer
+        x = self.combine(attention_values)
+        x = self.w_o(x)
+
+# feed forward network
 class FeedForward(nn.Module):
-    """
-    Args:
-        embed_dim: demension of embedding
-        d_ff: dimension of feed forward
-        dropout: dropout rate
-    """
-
     def __init__(self, embed_dim, d_ff=2048, dropout=0.1):
+        """
+        Args:
+            embed_dim: dimension of embeddings
+            d_ff: dimension of feed forward
+            dropout: dropout rate
+        """
         super().__init__()
         self.fc1 = nn.Linear(embed_dim, d_ff) # first linear transformation
         self.fc2 = nn.Linear(d_ff, embed_dim) # second linear transformation
